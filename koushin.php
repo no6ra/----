c5e6c7db9dce27662875ae7dd8c5fe89740eba3b
野村 <no6ra@speedmap-navi.com>
2014-08-02 23:08:45 +0900

    リダイレクト処理追加

--- koushin.php
+++ koushin.php
@@ -1,6 +1,5 @@
-<html>
-<body>
 <?
+header("Location:kanri.php");
 // データベースに接続
 if(!$con=mysql_connect('localhost', 'root', 'root')){
 echo"接続エラー";
@@ -31,5 +30,3 @@ mysql_close($con);
 // 登録完了メッセージの表示
 echo "更新完了";
 ?>
-</body>
-</html>
