d7661445e42f416490191449a051e3aeb1f54520
野村 <no6ra@speedmap-navi.com>
2014-08-04 21:46:40 +0900

    header処理

--- koushin.php
+++ koushin.php
@@ -1,32 +1,28 @@
-<?
-header("Location:kanri.php");
-// データベースに接続
+<?php
 if(!$con=mysql_connect('localhost', 'root', 'root')){
-echo"接続エラー";
-exit;
-} 
-
-// データベースを選択
-if(!mysql_select_db("newenquete",$con)){
-echo"データベース選択エラー";
 exit;
 }
+
+if(!mysql_select_db("newenquete",$con)){
+header("Location:dberror.php");
+
+exit;
+}
+
 $id = $_POST['id'];
 $gb = $_POST['gb'];
 $kaimono = $_POST['kaimono'];
 $chian = $_POST['chian'];
 $yachin = $_POST['yachin'];
 
-// UPDATE文を実行
 $sql = "update result set gb='$gb' , kaimono='$kaimono' , chian='$chian' , yachin='$yachin' where id =$id";
 if(!$res=mysql_query($sql)){
-echo "SQL実行時エラー";
+header("Location:sqlerror.php");
 exit;
 }
 
-// データベースから切断
 mysql_close($con);
 
-// 登録完了メッセージの表示
-echo "更新完了";
+header("Location:kanri.php");
+exit;
 ?>
