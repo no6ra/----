6367c518f9c5b0d9770829990d5249290876ddbf
野村 <no6ra@speedmap-navi.com>
2014-08-19 14:28:39 +0900

    htmlとphpを分けて記載し直しました。

--- kanri.php
+++ kanri.php
@@ -1,3 +1,4 @@
+
 <?php
 include_once('db.php');
 $link  = connectDB();
@@ -6,12 +7,19 @@ if(!$res=mysqli_query($link,"select id,gb, kaimono, chian, yachin from result"))
 print "SQLエラー<BR>";
 exit;
 }
+?>
 
-// 検索した結果を全部表示
-print "<table border=1>";
-print "<tr><td>良い、悪い</td><td>買物便利度</td><td>治安の良さ></td><td>賃料は適切でしたか？</td><td>更新</td><td>削除</td></tr>";
-while($row=mysqli_fetch_array($res)){
-print "<tr>";
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+       <meta charset="utf-8">
+       <title>アンケート回答結果</title>
+     </head>
+<body>
+<table border=1>
+<tr><td>良い、悪い</td><td>買物便利度</td><td>治安の良さ></td><td>賃料は適切でしたか？</td><td>更新</td><td>削除</td></tr>
+<?while($row=mysqli_fetch_array($res)){
+print"<tr>";
 print "<td>". htmlspecialchars($row["gb"],ENT_QUOTES) . "</td>";
 print "<td>". htmlspecialchars($row["kaimono"],ENT_QUOTES) . "</td>";
 print "<td>". htmlspecialchars($row["chian"],ENT_QUOTES) . "</td>";
@@ -28,11 +36,12 @@ print "<td><input type=submit value=削除></td>";
 print "</form>";
 print "</tr>";
 }
-print "</table>";
-
-// 結果セットの解放
+?>
+</table>
+<?
 mysqli_free_result($res);
 
-// データベースから切断
 mysqli_close($link);
 ?>
+</body>
+</html>
