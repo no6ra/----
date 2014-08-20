22b3cbaa970b065962c6e892d59b9ef13e4749db
野村 <no6ra@speedmap-navi.com>
2014-08-20 13:38:52 +0900

    htmlとphpを分けて修正し直しました！

--- kanri.php
+++ kanri.php
@@ -18,30 +18,28 @@ exit;
 <body>
 <table border=1>
 <tr><td>良い、悪い</td><td>買物便利度</td><td>治安の良さ></td><td>賃料は適切でしたか？</td><td>更新</td><td>削除</td></tr>
-<?while($row=mysqli_fetch_array($res)){
-print"<tr>";
-print "<td>". htmlspecialchars($row["gb"],ENT_QUOTES) . "</td>";
-print "<td>". htmlspecialchars($row["kaimono"],ENT_QUOTES) . "</td>";
-print "<td>". htmlspecialchars($row["chian"],ENT_QUOTES) . "</td>";
-print "<td>". htmlspecialchars($row["yachin"],ENT_QUOTES) . "</td>";
+<?while($row=mysqli_fetch_array($res)){?>
+<tr>
+<td><?php echo(htmlspecialchars($row["gb"],ENT_QUOTES))?></td>
+<td><?php echo(htmlspecialchars($row["kaimono"],ENT_QUOTES))?></td>
+<td><?php echo(htmlspecialchars($row["chian"],ENT_QUOTES))?></td>
+<td><?php echo(htmlspecialchars($row["yachin"],ENT_QUOTES))?></td>
 
-print "<form action=koushin_input.php method=post>";
-print "<input type=hidden name = id value=" . $row["id"] . ">";
-print "<td><input type=submit value=更新></td>";
-print "</form>";
+<form action=koushin_input.php method=post>
+<input type="hidden" name = "id" value=" . $row["id"] . ">
+<td><input type="submit" value="更新"></td>
+</form>
 
-print "<form action = sakujo.php method=post>";
-print "<input type = hidden name = id value=" . $row["id"] . ">";
-print "<td><input type=submit value=削除></td>";
-print "</form>";
-print "</tr>";
-}
-?>
+<form action = sakujo.php method=post>
+<input type = "hidden" name = "id" value=" . $row["id"] . ">
+<td><input type="submit" value="削除"></td>
+</form>
+</tr>
+<?}?>
 </table>
-<?
+</body>
+</html>
+<? 
 mysqli_free_result($res);
 
 mysqli_close($link);
-?>
-</body>
-</html>
