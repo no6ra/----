42c8314a70aea2aa34adec4227ce1bd7f5488069
野村 <no6ra@speedmap-navi.com>
2014-08-20 23:55:13 +0900

    ,ENT_QUOTESを削除しました！

--- kanri.php
+++ kanri.php
@@ -23,10 +23,10 @@ require "common.php";
 <tr><td>良い、悪い</td><td>買物便利度</td><td>治安の良さ></td><td>賃料は適切でしたか？</td><td>更新</td><td>削除</td></tr>
 <?while($row=mysqli_fetch_array($res)){?>
 <tr>
-<td><?php echo(h($row["gb"],ENT_QUOTES))?></td>
-<td><?php echo(h($row["kaimono"],ENT_QUOTES))?></td>
-<td><?php echo(h($row["chian"],ENT_QUOTES))?></td>
-<td><?php echo(h($row["yachin"],ENT_QUOTES))?></td>
+<td><?php echo(h($row["gb"]))?></td>
+<td><?php echo(h($row["kaimono"]))?></td>
+<td><?php echo(h($row["chian"]))?></td>
+<td><?php echo(h($row["yachin"]))?></td>
 
 <? print "<form action=koushin_input.php method=post>";
 print "<input type=hidden name = id value=" . $row["id"] . ">";
